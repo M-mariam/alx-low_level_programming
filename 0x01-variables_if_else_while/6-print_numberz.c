675b9900c19c0e38efd66748977797d6c4b15133
Mariam <mariamabdelaziz622@gmail.com>
2023-08-11 10:30:30 -0700

    numbrz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a
+ *
+ * Return: (0).
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
