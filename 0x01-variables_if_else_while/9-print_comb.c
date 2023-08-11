12bf896bef75853a00908bbdd15744da034f0ca3
Mariam <mariamabdelaziz622@gmail.com>
2023-08-11 14:15:01 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a
+ *
+ * Return: (0).
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+
+		digit0++;
+	}
+	putchar('\n');
+	return (0);
