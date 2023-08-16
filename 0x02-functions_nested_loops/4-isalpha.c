a0cb557dcb2e2ee0be27fdfc14f6e0610e725383
Mariam <mariamabdelaziz622@gmail.com>
2023-08-16 08:21:54 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  * Return: 1 if char is lowercase, otherwise 0.
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
